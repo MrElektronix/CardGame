6fd493a4aa8d372226f7f4fe5b99e4cb2b3d43f4
MrElektronix <milovanpelt@gmail.com>
2018-09-20 18:04:18 +0200

    setup

---
+++ Card Game/Assets/DropZone.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine.EventSystems;
+using UnityEngine;
+
+public class DropZone : MonoBehaviour, IDropHandler {
+
+    public void OnDrop(PointerEventData eventData)
+    {
+        Debug.Log(eventData.pointerDrag.name + " was dropped on " + gameObject.name);
+
+        Draggable d = eventData.pointerDrag.GetComponent<Draggable>();
+        if (d != null)
+        {
+            d.parentToReturnTo = this.transform;
+        }
+    }
+	
+}
